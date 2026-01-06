2f01174ca23e8403463ae2d00b8a72be99d2a64c
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2026-01-07 00:52:38 +0530

    Update vite.config.js

--- TraveloCity/vite.config.js
+++ TraveloCity/vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TraveloCity/', // <<=== Add this line
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  }
+});
